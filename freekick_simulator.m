7c517ff00bf6fae53b3c1421ac51bee0b5022e33
Pierre-Stephane Voltaire <psvoltaire96@gmail.com>
2019-04-14 14:05:52 -0400

    added goal post

--- freekick_simulator.m
+++ freekick_simulator.m
@@ -1,9 +1,11 @@
+
+ballsWeight=[[340,69],[390,69],[450,68.83185 ]];
+ 	 
 clear;
 
 disp("Free Kick Simulator");
 
 % Get input from the user
-ball_size = input('Please select the size of the ball (3, 4, 5): ');
 
 % Constant Variable
 AD = 1.2;       % Air Density at Sea Level (kg/m^3)
@@ -13,24 +15,25 @@ G = 1;              % Gravity
 D = AD * DC * cross_section; % Drag Calculation
 
 ball_mass = 2;      % Mass of the Ball (varies in size)
-foot_mass = 2;      % Mass of the Foot
+foot_mass = 1.43;      % Mass of the Foot
 e = 0.68;       % Coefficient 
 Vf1 = 1;            % Velocity of the foot before Kick
 Vf2 = 1;            % Velocity of the foot after Kick
 Vb1 = 0;        % Velocity of the ball before Kick (Constant value 0)
 Vb2 = (Vf1 * (foot_mass * (1+ e)) + Vb1 * (ball_mass - e * foot_mass)) / (foot_mass + ball_mass); % Velocity of the ball after Kick
 
-% Create Axis
-axis equal;
-axis([0 30 0 20 0 10]);
-grid on;
-
-% {Todo} Visualize the wall
-
-% {Todo} Visualize the goal post
-
-% {Todo} Bind the visual model to Axis
-
-% {Todo} Calculate the kick
-
-% {Todo} For loop for simulation
+%goal post
+l =  8;                                                 % Length
+w =  7.3;                                                 % Width
+h = 8;                                                 % Height
+X = [-1  -1   1   1  -1; -1  -1   1   1  -1]*l;
+Y = [3   5   5  3  3; 3   5   5  3  3]*w;
+Z = [ 1   1   1   1   1;  0   0   0   0   0]*h;
+figure(1)
+surf(X, Y, Z)                                           % Plot Walls
+hold on
+patch(X(1,:), Y(1,:), Z(1,:), 'y')                      % Plot Flat Roof
+hold off
+grid on
+axis equal
+axis([-25  25    -40.3  40.3    0  9])
